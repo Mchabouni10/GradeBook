49db6f712953d0e5d9f061e8b4af5c586cb86540
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-11-22 11:03:27 -0600

    gradebook home work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react"; 
+import "./App.css";
 
 function App() {
+  const studentNameRef = useRef("");
+  const gradeRef = useRef("");
+  const [studentName, setStudentName] = useState("");
+  const [grade, setGrade] = useState("");
+  const [form, setForm] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setForm([...form, { name: studentName, grade }]);
+    setStudentName("");
+    setGrade("");
+    // Clear the ref values
+    studentNameRef.current = "";
+    gradeRef.current = "";
+  };
+
+  const calculateMean = () => {  //function that calculate the mean
+    if (form.length === 0) return 0;
+    const totalGrades = form.reduce((acc, row) => acc + parseFloat(row.grade), 0);
+    return totalGrades / form.length;
+  };
+
+  const heighGrade = () => { //function that calculate the high grade
+    if (form.length === 0) return 0;
+    const topGrade = Math.max(...form.map((row) => parseFloat(row.grade)));
+    return topGrade;
+  };
+
+  const lowestGrade = () => { //function that calculate the lowest grade
+    if (form.length === 0) return 0;
+    const lowGrade = Math.min(...form.map((row) => parseFloat(row.grade)));
+    return lowGrade;
+  };
+
+  const displayList = () => (  //function to display my form 
+    <tbody>
+      {form.map((row, index) => (
+        <tr key={index}>
+          <td className="student-info">
+            <div className="student-name">
+              Student name: <strong>{row.name}</strong>
+            </div>
+            <div className="student-grade">
+              Student grade: <strong>{row.grade}/100</strong>
+            </div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <div className="input-row">
+            <input
+              type="text"
+              value={studentName}
+              onChange={(e) => setStudentName(e.target.value)}
+              placeholder="Student name"
+              className="student-name"
+            />
+            <input
+              type="number"
+              value={grade}
+              onChange={(e) => setGrade(e.target.value)}
+              placeholder="Grade btwn 0-100"
+              className="student-grade"
+            />
+          </div>
+          <input type="submit" value="Submit Form" className="submit-button" />
+        </form>
+      </div>
+      <div className="display">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Mean Grade: </strong> {calculateMean()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <strong>Maximum Grade: </strong>
+          {heighGrade()}
+        </p>
+        <p>
+          <strong>Minimum Grade: </strong>
+          {lowestGrade()}
+        </p>
+        <p>{displayList()}</p>
+      </div>
+    </>
   );
 }
 
